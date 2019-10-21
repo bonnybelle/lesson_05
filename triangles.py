f9f0dda8f00154be9be8ed97125ad736a2b3d261
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2019-10-21 13:36:37 +0300

    Update and rename two.py to triangles.py

--- triangles.py
+++ triangles.py
@@ -8,3 +8,13 @@ for i in range(rows):
     print()
 print()
 
+
+rows = int(input('Введите высоту: '))
+for i in range(rows):
+    for j in range(rows * 2):
+        if rows - 1 - i <= j <= rows - 1 + i:
+            print('*  ', end='')
+        else:
+            print('   ', end='')
+    print()
+print()
