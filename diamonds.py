1b431b0c063e8041addf88fa858c0e36cfbfc48a
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2019-10-21 20:28:43 +0300

    Update diamonds.py

--- diamonds.py
+++ diamonds.py
@@ -1,5 +1,6 @@
-k = int(input('Введите высоту: '))
-h = int(k/2+1/2)
+# C
+k = int(input('Введите высоту для ромба C (нечётное значение): '))
+h = int(k / 2 + 1 / 2)
 print(h)
 
 for i in range(h):
@@ -9,18 +10,32 @@ for i in range(h):
         else:
             print('   ', end='')
     print()
-   
-for i in range(h + 1, k):
+
+for i in range(h + 1, k + 1):
     for j in range(k + 1):
-        if h * 2 <= k:                                                  # в случае, когда введенная высота чётная
-            if j == i - h or j == h - 1 or j == k + h - i - 2:
-                print('*  ', end='')
-            else:
-                print('   ', end='')
-        elif h * 2 >= k:                                                # в случае, когда введенная высота нечётная
-            if j == i - h or j == h - 1 or j == k + h - i - 1:
-                print('*  ', end='')
-            else:
-                print('   ', end='')
+        if j == i - h or j == k + h - i - 1:
+            print('*  ', end='')
+        else:
+            print('   ', end='')
+    print()
+print()
+
+
+# D
+k = int(input('Введите высоту для ромба D (нечётное значение): '))
+h = int(k / 2 + 1 / 2)
+for i in range(h):
+    for j in range(k):
+        if h - 1 - i <= j <= h - 1 + i:
+            print('*  ', end='')
+        else:
+            print('   ', end='')
+    print()
+for i in range(h + 1, k + 1):
+    for j in range(k + 1):
+        if j == i - h or j == h - 1 or j == k + h - i - 1:
+            print('*  ', end='')
+        else:
+            print('   ', end='')
     print()
 print()
